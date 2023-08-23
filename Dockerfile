f52f5b11624410400bed65904660818f47baef8f
leahMontag <mirimontag@gmail.com>
2023-08-23 11:17:01 +0300

    update project

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,10 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
+EXPOSE 5000
+
+ENV ROOM_FILES_PATH /app/room_files
+
+RUN mkdir room_files
+
 CMD [ "python", "./chatApp.py" ]
--- chatApp.py
+++ chatApp.py
@@ -1,58 +1,118 @@
-import re
-from flask import Flask, render_template, request, redirect
-import csv 
+from flask import Flask, render_template, request, redirect, session, jsonify
+import csv
+import os
+import base64
+from datetime import datetime
+
 app = Flask(__name__)
-# IS_DEV = app.env == 'development'
-# relative_path = os.environ.get('RELATIVE_PATH', './templates')
+app.secret_key = 'your_secret_key'  # Set a secret key for session management
+
+# Retrieve the room files path from environment variable
+#room_files_path = os.getenv('ROOM_FILES_PATH')
+room_files_path = "rooms/"
+
+# Helper functions for user authentication
+def encode_password(password):
+    encoded_bytes = base64.b64encode(password.encode('utf-8'))
+    return encoded_bytes.decode('utf-8')
+
+
+def decode_password(encoded_password):
+    decoded_bytes = base64.b64decode(encoded_password.encode('utf-8'))
+    return decoded_bytes.decode('utf-8')
+
+
+def check_user_credentials(username, password):
+    with open('users.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == username and decode_password(row[1]) == password:
+                return True
+    return False
+
+
+
+
+# Routes
+@app.route('/')
+def index():
+    return "WELCOME"
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def homePage():
+def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        f = open('users.csv', 'a')
-        writer = csv.writer(f)
-        writer.writerow([username,password])
-        f.close()
+        encoded_password = encode_password(password)
+        
+        # Save user details to the CSV file
+        with open('readme.txt', 'w') as f:
+            f.write('Create a new text file!')
+        with open('users.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([username, encoded_password])
+        
         return redirect('/login')
-    if request.method == 'GET':
-        return render_template('register.html')
+    return render_template('register.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
-def loginPage():
+def login():
     if request.method == 'POST':
-        return redirect('/lobby')
-    if request.method == 'GET':
-        return render_template('login.html')
+        username = request.form['username']
+        password = request.form['password']
+        
+        if check_user_credentials(username, password):
+            session['username'] = username
+            return redirect('/lobby')
+        else:
+            return "Invalid credentials. Please try again."
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
+
 
 @app.route('/lobby', methods=['GET', 'POST'])
 def lobby():
-    # if request.method == 'POST':
-    #     return redirect('/chat')
-    # if request.method == 'GET':
-        return render_template('lobby.html')   
+    if 'username' in session:
+        if request.method == 'POST':
+            room_name = request.form['new_room']
+            #todo:create room.txt
+            print("CREATED NEW ROOM NAMED: " + room_name )
 
-# @app.route('/chat')
-# def chat_room():
-#     return render_template('chat.html')    
-
-
-
+        return render_template('lobby.html')  
+    else:
+        return redirect('/login')
 
 
-#def validation():
+@app.route('/chat/<room>', methods=['GET', 'POST'])
+def chat(room):
+    if 'username' in session:
+        if request.method == 'POST':
+            message = request.form['msg']
+            print("MESSAGE RECEIVED IN CHAT " + message )
+        return render_template('chat.html', room=room)
+    else:
+        return redirect('/login')
 
 
+@app.route('/api/chat/<room>', methods=['POST'])
+def update_chat(room):
+    message = request.json['message']
+    username = session['username']
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Append the message to the room's unique .txt file
+    with open(f'{room_files_path}/{room}.txt', 'a') as file:
+        file.write(f'[{timestamp}] {username}: {message}\n')
+    
+    return jsonify({'status': 'success'})
 
 
-
-
-#if __name__ == '__main__':
- #   app.run(host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    # guaranteed to not be run on a production server
-    # assert os.path.exists('.env')  # for other environment variables...
-    # os.environ['FLASK_ENV'] = 'development'  # HARD CODE since default is production
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
