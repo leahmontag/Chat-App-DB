6b469277e0dbbf031f7f3fc3b0468ba675fa6173
EkshteinChana <c8508303@gmail.com>
2023-08-29 13:16:21 +0300

    limit the resources

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 docker volume create chat-data
-# docker build -t chat_img . 
-# docker run -v chat-data:/chatApp/data -d -p 5000:5000 --name chat_con chat_img
-#Conf limits
 docker build -t chat_img .
 docker run -v chat-data:/chatApp/data -d -p 5000:5000 --name chat_con --memory=1g --memory-reservation=512m --cpus=1 --cpuset-cpus=2 chat_img
 
