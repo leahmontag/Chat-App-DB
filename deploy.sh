05c4aeff8cd4553120165859fdfce4a67245097f
EkshteinChana <c8508303@gmail.com>
2023-08-30 12:31:46 +0300

    deploy.sh with errs handling fixed

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ set -e
 
 version=$1
 commit_hash=$2
-if [ "" -eq $version] || ["" -eq $commit_hash ]; then
+if [ "" =  "$version" ] || [ "" = "$commit_hash" ]; then
     echo "Error: Missing parameters, Try run again with the version did you want and the commit hash"
     exit 1
 fi
-img_name_and_tag="chat_img:v${version}"
+echo "succes"
+img_name_and_tag="chat_img:${version}"
 docker volume create chat-data
 docker build -t  $img_name_and_tag .
 git tag ${version} ${commit_hash} 
