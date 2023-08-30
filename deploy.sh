8cdb927ba36c08399c8ecfec9243092ae8c8445b
EkshteinChana <c8508303@gmail.com>
2023-08-30 11:27:33 +0300

    Auto tag  by deploy.sh without Errs handling

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,3 @@ docker volume create chat-data
 docker build -t  $img_name_and_tag .
 git tag ${version} ${commit_hash} 
 git push origin ${version}
-
-# Write a script called deploy.sh that gets from the user version 
-# and commit hash and automates tagging and building image and tagging and pushing to your github repository.
-# Don’t forget error handling!
