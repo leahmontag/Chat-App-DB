0bf920d656f19e83963efd01ba522efc40a31b36
EkshteinChana <c8508303@gmail.com>
2023-08-30 12:13:20 +0300

    deploy.sh with errors handling

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f chat_con
-docker rmi -f chat_img
+docker rmi -f chat_img:v0.1.0
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-read version commit_hash
-img_name_and_tag="chat_img:${version}"
+
+#Stop the Execution on the First Error 
+set -e  
+
+version=$1
+commit_hash=$2
+if [ "" -eq $version] || ["" -eq $commit_hash ]; then
+    echo "Error: Missing parameters, Try run again with the version did you want and the commit hash"
+    exit 1
+fi
+img_name_and_tag="chat_img:v${version}"
 docker volume create chat-data
 docker build -t  $img_name_and_tag .
 git tag ${version} ${commit_hash} 
